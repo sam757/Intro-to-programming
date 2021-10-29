8e40c477d4cedc967ba77896dbd9b973458c92bd
sam757 <56123830+sam757@users.noreply.github.com>
2021-10-29 02:37:34 +0200

    Add files via upload

---
+++ IPlab2/src/problem1.java
@@ -0,0 +1,16 @@
+
+public class problem1 {
+
+	public static void main(String[] args) {
+		double radius;
+		double area;
+		
+		radius = 25;
+		
+		area = radius * radius * 3.14159;
+		
+		System.out.println("The area for the circle of radius" + radius + " is " + area);
+
+	}
+
+}
